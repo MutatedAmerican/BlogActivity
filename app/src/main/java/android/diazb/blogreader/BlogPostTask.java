22d7256bd8ea8a34330abadececb0caf29382547
Bryan Diaz <bryandiaz2010@yahoo.com>
2014-10-03 15:02:58 -0700

    Starter BlogBuster

--- app/src/main/java/android/diazb/blogreader/BlogPostTask.java
+++ app/src/main/java/android/diazb/blogreader/BlogPostTask.java
@@ -6,19 +6,34 @@ import android.util.Log;
 
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class BlogPostTask extends AsyncTask<Activity, Void, JSONObject>{
     @Override
-    protected JSONObject doInBackground(Activity... params) {
+    protected JSONObject doInBackground(Activity... activities) {
 
         try{
             URL blogFeedUrl= new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count10");
+
+            HttpURLConnection connection=(HttpURLConnection)blogFeedUrl.openConnection();
+            connection.connect();
+
+            int responseCode= connection.getResponseCode();
+
+            if (responseCode==HttpURLConnection.HTTP_OK){
+
+            }
         }
 
         catch (MalformedURLException error) {
             Log.e("BlogPostTask", "Malformed URL:" + error);
+        }
+
+        catch (IOException error) {
+            Log.e("BlogPostTask", "IO Exception:" + error);
         }
         return null;
     }
