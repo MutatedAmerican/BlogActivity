7de1658c529f23daf3854edfbf42994008e9cfa6
Bryan Diaz <bryandiaz2010@yahoo.com>
2014-10-09 15:02:42 -0700

    Video #25-BlogReader

--- app/src/main/java/android/diazb/blogreader/BlogPostParser.java
+++ app/src/main/java/android/diazb/blogreader/BlogPostParser.java
@@ -2,19 +2,23 @@ package android.diazb.blogreader;
 
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.InputMismatchException;
+import java.util.ArrayList;
 
 public class BlogPostParser {
     private static BlogPostParser parser;
+    public ArrayList<BlogPost> posts;
 
     private BlogPostParser(){
-
+        posts=new ArrayList<BlogPost>();
     }
 
     public static BlogPostParser get(){
@@ -33,13 +37,38 @@ public class BlogPostParser {
 
         try{
             while ((line= reader.readLine())!= null){
-
+                builder.append(line);
             }
+            JSONTokener jsonTokener=new JSONTokener(builder.toString());
+            jsonObject= new JSONObject(jsonTokener);
         }
 
         catch(IOException error){
             Log.e("BlogPostParser", "IOException: "+ error);
         }
-        return null;
+
+        catch(JSONException error){
+            Log.e("BlogPostParser", "JSON Exception: "+ error);
+        }
+        return jsonObject;
+    }
+
+    public void readFeed(JSONObject jsonObject){
+        try{
+            JSONArray jsonPosts= jsonObject.getJSONArray("posts");
+
+            for(int index=0; index<jsonObject.length(); index++){
+                JSONObject post=jsonPosts.getJSONObject(index);
+
+                String title=post.getString("title");
+                String url=post.getString("url");
+
+                BlogPost blogPost=new BlogPost(title,url);
+                posts.add(blogPost);
+            }
+        }
+        catch(JSONException error){
+            Log.e("BlogPostParser", "JSONException: " + error);
+        }
     }
 }
--- app/src/main/java/android/diazb/blogreader/BlogPostTask.java
+++ app/src/main/java/android/diazb/blogreader/BlogPostTask.java
@@ -3,6 +3,8 @@ package android.diazb.blogreader;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+
 import org.json.JSONObject;
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -10,8 +12,12 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class BlogPostTask extends AsyncTask<Activity, Void, JSONObject>{
+    private Activity activity;
     @Override
     protected JSONObject doInBackground(Activity... activities) {
+        activity=activities[0];
+        JSONObject jsonObject = null;
+
         try{
             URL blogFeedUrl= new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count10");
 
@@ -22,6 +28,7 @@ public class BlogPostTask extends AsyncTask<Activity, Void, JSONObject>{
 
             if (responseCode==HttpURLConnection.HTTP_OK){
                 Log.i("BlogPostTask", "Successful Connection" + responseCode);
+                jsonObject=BlogPostParser.get().parse(connection.getInputStream());
             }
         }
 
@@ -32,6 +39,13 @@ public class BlogPostTask extends AsyncTask<Activity, Void, JSONObject>{
         catch (IOException error) {
             Log.e("BlogPostTask", "IO Exception:" + error);
         }
-        return null;
+        return jsonObject;
+    }
+
+    @Override
+    protected void onPostExecute(JSONObject jsonObject) {
+
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayStrings);
+        listView.setAdapter(adapter);
     }
 }
