281db1aab1c649501a1421d4dfea0b67ecd69b10
Bryan Diaz <bryandiaz2010@yahoo.com>
2014-10-19 10:33:13 -0700

    Comment on Starter Milestone -As of now, it actually seems still pretty confusing to understand the coding, even with the coding and the tutorial. I'm hoping you can tell us more, in details, of the steps in class from now on. Other than its confusion, nothing comes into mind, al seems well in the end....for now of course.

--- app/src/main/java/android/diazb/blogreader/BlogActivity.java
+++ app/src/main/java/android/diazb/blogreader/BlogActivity.java
@@ -1,6 +1,8 @@
 package android.diazb.blogreader;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -30,6 +32,12 @@ public class BlogActivity extends Activity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Log.i("BlogActivity", "Title" + BlogPostParser.get().posts.get(position).title);
+
+                Intent intent= new Intent(getApplicationContext(), BlogWebActivity.class);
+                Uri blogUri= Uri.parse(BlogPostParser.get().posts.get(position).url);
+                intent.setData(blogUri);
+
+                startActivity(intent);
             }
         });
 
--- app/src/main/java/android/diazb/blogreader/BlogPostAdapter.java
+++ app/src/main/java/android/diazb/blogreader/BlogPostAdapter.java
@@ -20,7 +20,7 @@ public class BlogPostAdapter extends ArrayAdapter<BlogPost> {
         BlogPost post= getItem(position);
 
         if(convertView== null){
-            convertView= LayoutInflater.from(getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+            convertView= LayoutInflater.from(getContext()).inflate(android.R.layout.simple_list_item_activated_2, parent, false);
         }
 
         TextView textView= (TextView)convertView.findViewById(android.R.id.text1);
