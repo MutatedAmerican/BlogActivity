281fe034b4e130bef2eff92b01dc0530c4bf3d74
Bryan Diaz <bryandiaz2010@yahoo.com>
2014-10-24 14:59:32 -0700

    Intermediate Milestone 7 (incomplete, need asssitance)

--- app/src/main/java/android/diazb/blogreader/BlogWebActivity.java
+++ app/src/main/java/android/diazb/blogreader/BlogWebActivity.java
@@ -5,17 +5,24 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.widget.ProgressBar;
 
 public class BlogWebActivity extends Activity{
+
+    protected ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog_web);
 
+        progressBar=(ProgressBar)findViewById(R.id.ProgressBar);
+
         Intent intent= getIntent();
         Uri blogUri= intent.getData();
 
         WebView webView= (WebView)findViewById(R.id.webView);
+        //WebView.setEmptyView(progressBar);
         webView.loadUrl(blogUri.toString());
     }
 }
